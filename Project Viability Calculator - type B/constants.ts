20f5a0b691afbb623c15478d9633395a611865c5
1WIR3 <147526257+1WIR3@users.noreply.github.com>
2025-10-01 19:33:59 -0400

    Create constants.ts

---
+++ Project Viability Calculator - type B/constants.ts
@@ -0,0 +1,8 @@
+export const CONSULTANT_RATES: { [key: string]: { salary: number, fixedCost: number } } = {
+  'Senior Strategy Consultant': { salary: 150000, fixedCost: 5000 },
+  'Standard Strategy Consultant': { salary: 100000, fixedCost: 5000 },
+  'Senior IT Consultant': { salary: 130000, fixedCost: 5000 },
+  'Standard IT Consultant': { salary: 90000, fixedCost: 5000 },
+};
+
+export const WORKING_DAYS_PER_YEAR = 220;
